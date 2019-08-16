39cec38ab6647b6641da02ae870c842e0f95a29d
Vladimir <lvpwolf@mail.ru>
2019-08-16 20:54:33 +0300

    pyda_1_4__homework

---
+++ libs/exchange.py
@@ -0,0 +1,88 @@
+import requests
+
+class Rate:
+    def __init__(self, format_='value', diff_=False):
+        self.format = format_
+        self.diff = diff_
+    
+    def exchange_rates(self):
+        """
+        Возвращает ответ сервиса с информацией о валютах в виде:
+        
+        {
+            'AMD': {
+                'CharCode': 'AMD',
+                'ID': 'R01060',
+                'Name': 'Армянских драмов',
+                'Nominal': 100,
+                'NumCode': '051',
+                'Previous': 14.103,
+                'Value': 14.0879
+                },
+            ...
+        }
+        """
+        r = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
+        return r.json()['Valute']
+    
+    def make_format(self, currency):
+        """
+        Возвращает информацию о валюте currency в двух вариантах:
+        - полная информация о валюте при self.format = 'full':
+        Rate('full').make_format('EUR')
+        {
+            'CharCode': 'EUR',
+            'ID': 'R01239',
+            'Name': 'Евро',
+            'Nominal': 1,
+            'NumCode': '978',
+            'Previous': 79.6765,
+            'Value': 79.4966
+        }
+        
+        Rate('value').make_format('EUR')
+        79.4966
+        """
+        response = self.exchange_rates()
+        
+        if currency in response:
+            if self.format == 'full':
+                return response[currency]
+            elif self.format == 'value':
+                if self.diff:
+                    return self.make_diff(round(response[currency]['Value'] - response[currency]['Previous'],4))
+                    
+                return response[currency]['Value']
+        
+        return 'Error'
+        
+    def make_diff(self, delta):
+        if delta < 0:
+            return 'Курс снизился на ' + str(abs(delta))
+        elif delta > 0:
+            return 'Курс вырос на ' + str(delta)
+        
+        return 'Курс остался прежним'
+    
+    def eur(self):
+        """Возвращает курс евро на сегодня в формате self.format"""
+        return self.make_format('EUR')
+    
+    def usd(self):
+        """Возвращает курс доллара на сегодня в формате self.format"""
+        return self.make_format('USD')
+		
+class Employee:
+    def __init__(self, name, seniority):
+        self.name = name
+        self.seniority = seniority
+        
+        self.grade = 1
+    
+    def grade_up(self):
+        """Повышает уровень сотрудника"""
+        self.grade += 1
+    
+    def publish_grade(self):
+        """Публикация результатов аккредитации сотрудников"""
+        print(self.name, self.grade)
